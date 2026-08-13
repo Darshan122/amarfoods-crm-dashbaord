640714b7a71d1839bc5e3925a8b1824c8303fff1
Darshan122 <dzalavadiya122@gmail.com>
2026-08-13 17:29:41 +0530

    Update local storage key to v6 for clean buyer order

--- lib/providers/buyer_provider.dart
+++ lib/providers/buyer_provider.dart
@@ -79,7 +79,7 @@ class BuyerProvider extends ChangeNotifier {
     loadBuyers(forceRefresh: true);
   }
 
-  static const String _localBuyersKey = 'amar_crm_local_buyers_v5';
+  static const String _localBuyersKey = 'amar_crm_local_buyers_v6';
 
   Future<void> _saveLocalBuyers() async {
     try {
